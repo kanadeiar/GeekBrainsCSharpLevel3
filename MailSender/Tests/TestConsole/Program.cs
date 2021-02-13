104ef02619af7213133f6b7be936fbe60117bc07
kanadeiar <kanadeiar@gmail.com>
2021-02-13 22:14:14 +0400

    Письмо с тестовой консоли отправляется благополучно

--- MailSender/Tests/TestConsole/Program.cs
+++ MailSender/Tests/TestConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace TestConsole
 {
@@ -6,7 +8,38 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Тестовая консоль";
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            ///////////////////////////////////////////////////////////////////////////////
+            var from = new MailAddress("kanadeiar@yandex.ru", "Андрей");
+            var to = new MailAddress("kanadeiar@gmail.com", "Андрей");
+            var message = new MailMessage(from, to);
+            message.Subject = "Заголовок";
+            message.Body = "Текст письма";
+
+            ///////////////////////////////////////////////////////////////////////////////
+            var client = new SmtpClient("smtp.yandex.ru", 587);
+            client.EnableSsl = true;
+            
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "kanadeiar@yandex.ru",
+                Password = "*",
+            };
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            
+
+            ///////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("Нажмите любую кнопку ...");
+            Console.ReadKey();
         }
     }
 }
