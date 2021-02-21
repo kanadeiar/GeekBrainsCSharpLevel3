c7dae8e76b5d37df08fd002f1efe47d9ea629d8f
kanadeiar <kanadeiar@gmail.com>
2021-02-21 13:17:05 +0400

    Добавлен сервис и кнопка немедленной отправки почты, но пока не работает

---
+++ MailSender/MailSender/Services/SmtpSender.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Services
+{
+    /// <summary> Боевой сервис отправки почты без шифрования пароля </summary>
+    public class SmtpSender
+    {
+        private readonly string _address;
+        private readonly int _port;
+        private readonly bool _useSsl;
+        private readonly string _login;
+        private readonly string _password;
+        /// <summary> Конструктор боевого сервиса отправки по smtp без шифрования пароля</summary>
+        /// <param name="address">адрес</param>
+        /// <param name="port">порт</param>
+        /// <param name="useSsl">шифрование</param>
+        /// <param name="login">логин</param>
+        /// <param name="password">пароль</param>
+        public SmtpSender(string address, int port, bool useSsl, string login, string password)
+        {
+            _address = address;
+            _port = port;
+            _useSsl = useSsl;
+            _login = login;
+            _password = password;
+        }
+        /// <summary> Отправка реального письма по протоколу smtp </summary>
+        /// <param name="From">отправитель</param>
+        /// <param name="To">получатель</param>
+        /// <param name="Subject">заголовок</param>
+        /// <param name="Message">сообщение</param>
+        public void Send(string From, string To, string Subject, string Message)
+        {
+            var message = new MailMessage(From, To)
+            {
+                Subject = Subject,
+                Body = Message,
+            };
+            var client = new SmtpClient(_address, _port)
+            {
+                EnableSsl = _useSsl,
+                Credentials = new NetworkCredential(_login, _password),
+            };
+            client.Send(message);
+        }
+    }
+}
--- MailSender/MailSender/WpfMailSender.xaml.cs
+++ MailSender/MailSender/WpfMailSender.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using MailSender.Data;
 using MailSender.Models;
+using MailSender.Services;
 using MailSender.Windows;
 
 namespace MailSender
@@ -82,6 +85,28 @@ namespace MailSender
             TestData.Servers.Remove(server);
             ComboBoxServers.Items.Refresh();
             ComboBoxServers.SelectedItem = server;
+        }
+        private void ButtonSendNow_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!(ComboBoxServers.SelectedItem is Server server)) return;
+            if (!(ComboBoxSenders.SelectedItem is Sender mySender)) return;
+            if (!(ListBoxMessages.SelectedItem is Message message)) return;
+            if (!(DataGridRecipients.SelectedItem is Recipient recipient)) return;
+
+            var mailSender = new SmtpSender(server.Address, server.Port, server.UseSsl, server.Login, server.Password);
+
+            try
+            {
+                var timer = Stopwatch.StartNew(); 
+                mailSender.Send(mySender.Address, recipient.Address, message.Subject, message.Text);
+                timer.Stop();
+                MessageBox.Show($"Почтовое сообщение успешно отправлено за {timer.Elapsed.TotalSeconds:0.##} секунд", "Выполнено", MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка отправки почты:\n" + ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
