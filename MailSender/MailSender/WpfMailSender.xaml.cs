ae275950e0240818fbe2b3c59c5e11c9f4320bd1
kanadeiar <kanadeiar@gmail.com>
2021-02-21 12:43:28 +0400

    Окно и функция добавления, редактирования и удаления сервера

--- MailSender/MailSender/WpfMailSender.xaml.cs
+++ MailSender/MailSender/WpfMailSender.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using MailSender.Data;
+using MailSender.Models;
+using MailSender.Windows;
 
 namespace MailSender
 {
@@ -12,5 +16,72 @@ namespace MailSender
 
 
         private void Exit_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void ButtonAddServer_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!ServerEditWindow.Create(
+                out var name,
+                out var address,
+                out var port,
+                out var ssl,
+                out var description,
+                out var login,
+                out var password))
+                return;
+            var server = new Server
+            {
+                Id = TestData.Servers
+                    .DefaultIfEmpty()
+                    .Max(s => s.Id) + 1,
+                Name = name,
+                Address = address,
+                Port = port,
+                UseSsl = ssl,
+                Description = description,
+                Login = login,
+                Password = password,
+            };
+            TestData.Servers.Add(server);
+            ComboBoxServers.Items.Refresh();
+            ComboBoxServers.SelectedItem = server;
+        }
+        private void ButtonEditServer_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!(ComboBoxServers.SelectedItem is Server server))
+                return;
+            var name = server.Name;
+            var address = server.Address;
+            var port = server.Port;
+            var ssl = server.UseSsl;
+            var description = server.Description;
+            var login = server.Login;
+            var password = server.Password;
+            if (!ServerEditWindow.ShowDialog("Редактирование почтового сервера", 
+                ref name,
+                ref address,
+                ref port,
+                ref ssl,
+                ref description,
+                ref login,
+                ref password))
+                return;
+            server.Name = name;
+            server.Address = address;
+            server.Port = port;
+            server.UseSsl = ssl;
+            server.Description = description;
+            server.Login = login;
+            server.Password = password;
+            ComboBoxServers.Items.Refresh();
+            ComboBoxServers.SelectedItem = server;
+        }
+        private void ButtonDeleteServer_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!(ComboBoxServers.SelectedItem is Server server))
+                return;
+            TestData.Servers.Remove(server);
+            ComboBoxServers.Items.Refresh();
+            ComboBoxServers.SelectedItem = server;
+        }
     }
 }
