57bf5dbf848762d2bd5be752dfc0fb3a28383134
kanadeiar <kanadeiar@gmail.com>
2021-02-22 09:23:49 +0400

    Добавлена команда справка приложения

--- MailSender/MailSender/ViewModels/WpfMailSenderViewModel.cs
+++ MailSender/MailSender/ViewModels/WpfMailSenderViewModel.cs
@@ -1,4 +1,7 @@
-﻿using MailSender.ViewModels.Base;
+﻿using System.Windows;
+using System.Windows.Input;
+using MailSender.Infrastructure.Commands;
+using MailSender.ViewModels.Base;
 
 namespace MailSender.ViewModels
 {
@@ -22,5 +25,17 @@ namespace MailSender.ViewModels
             get => _description;
             set => Set(ref _description, value);
         }
+
+        #region Команды
+
+        private ICommand _showDialogCommand;
+        /// <summary> Команда показа простого диалогового окна приложения </summary>
+        public ICommand ShowDialogCommand => _showDialogCommand ??= new LambdaCommand(OnShowDialogCommandExecute);
+        private void OnShowDialogCommandExecute(object p)
+        {
+            var message = p as string ?? "Привет, Мир!";
+            MessageBox.Show(message, "Сообщение приложения", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        #endregion
     }
 }
