d9505edfdb796e1471e82672514937af399bc3ee
kanadeiar <kanadeiar@gmail.com>
2021-02-22 08:46:50 +0400

    Успешное внедрение базы MVVM

---
+++ MailSender/MailSender/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MailSender.ViewModels.Base
+{
+    /// <summary> Базовое определение вьюмодели </summary>
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return;
+            field = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
---
+++ MailSender/MailSender/ViewModels/WpfMailSenderViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MailSender.ViewModels.Base;
+
+namespace MailSender.ViewModels
+{
+    /// <summary> Вьюмодель главного окна приложения </summary>
+    class WpfMailSenderViewModel : ViewModel
+    {
+        public WpfMailSenderViewModel()
+        {
+        }
+        private string _title = "Geekbrains. Домашнее задание №3. MVVM.";
+        /// <summary> Заголовок главного окна </summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        private string _description = "Geekbrains. Домашнее задание №3. Разработка WPF-приложений с использованием шаблона MVVM на примере MVVM Light Toolkit";
+        /// <summary> Описание приложения </summary>
+        public string Description
+        {
+            get => _description;
+            set => Set(ref _description, value);
+        }
+    }
+}
