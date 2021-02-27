ca5fe7a18c620f5f04b0d9f98d3ab26a9e5377d1
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-02-27 22:56:09 +0400

    Добавление вьюхи редактирования получателя писем

---
+++ MailSender/MailSender/Views/RecipientEditorView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MailSender.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для RecipientEditorView.xaml
+    /// </summary>
+    public partial class RecipientEditorView : UserControl
+    {
+        public RecipientEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
