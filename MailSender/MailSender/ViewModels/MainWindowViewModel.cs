7105c8e7a742ab121bcf15a11158e5444b454a45
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-02-27 19:07:53 +0400

    Небольшая починка отправки писем и отображения списка получателей

--- MailSender/MailSender/ViewModels/MainWindowViewModel.cs
+++ MailSender/MailSender/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Timers;
 using MailSender.lib.Interfaces;
